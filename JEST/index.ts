b31f1a29d78cb54714cb2f6fceecdae4192c04fc
Giuliano <giuilianogarcia2826@gmail.com>
2024-05-14 23:05:30 -0300

    SpyOn in jest

---
+++ JEST/index.ts
@@ -0,0 +1,18 @@
+console.log('We are Testing you');
+/**
+ * .test.ts: Esta convenção é frequentemente usada para arquivos 
+ * de teste unitário. Um teste unitário verifica uma unidade 
+ * isolada de código, como uma função ou método, para garantir 
+ * que ela funcione conforme o esperado. Geralmente, esses 
+ * testes são mais focados em verificar a lógica interna de 
+ * pequenas partes do código.
+ * 
+ *.spec.ts: Esta convenção é mais frequentemente associada a 
+ testes de especificações (specs) ou testes de comportamento. 
+ Esses testes se concentram em verificar o comportamento geral 
+ de uma funcionalidade ou componente. Eles podem testar 
+ interações entre diferentes partes do sistema e garantir que o 
+ sistema funcione corretamente como um todo. 
+ 
+ 
+ */
